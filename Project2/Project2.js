2e06c286b69c794db420bd3da99acc296e203d52
K.Preetham Reddy <kallemp.reddy9@gmail.com>
2024-09-21 22:31:16 +0530

    Suit Landing Page(HTML,CSS,JavaScript)

---
+++ Project2/Project2.js
@@ -0,0 +1,5 @@
+const bon=document.querySelectorAll("button")
+bon.forEach(bon=>{bon.addEventListener("click",function(){
+    const url="https://www.linkedin.com/in/preetham-reddy-65794b297/";
+    window.open(url,"_blank");
+})})
